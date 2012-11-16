ce65912ce50b4196726508ac46d05e7cfd8b3a16
Bryan Osterkamp <bryan.osterkamp@gmail.com>
2012-11-16 08:37:57 -0600

    Initial Commit

---
+++ Oakwood/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Oakwood
+//
+//  Created by The Osterkamps on 11/16/12.
+//  Copyright (c) 2012 Oakwood Baptist Church. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Oakwood/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Oakwood
+//
+//  Created by The Osterkamps on 11/16/12.
+//  Copyright (c) 2012 Oakwood Baptist Church. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
